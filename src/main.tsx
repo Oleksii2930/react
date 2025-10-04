ed9d4c4a50a90f84c4ecdd7383ecc05ea9c02de5
Олексій2930 <korobcukdenis30@gmail.com>
2025-10-04 10:10:00 +0300

    hw3 task2

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,20 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import { RouterProvider} from "react-router";
-import {routes} from "./router/routes.tsx";
-
+import { BrowserRouter, Routes, Route} from "react-router";
+import UsersComponent from "./components/UsersComponent.tsx";
+import PostsComponent from "./components/PostsComponent.tsx";
+import CommentsComponent from "./components/CommentsComponent.tsx";
+import App from "./App";
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes}/>);
+    .render(<BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<App/>}>
+                <Route path={'users'} element={<UsersComponent/>}/>
+                <Route path={'posts'} element={<PostsComponent/>}/>
+                <Route path={'comments'} element={<CommentsComponent/>}/>
+                <Route path={'products'} element={<PostsComponent/>}/>
+            </Route>
+
+        </Routes>
+    </BrowserRouter>);
