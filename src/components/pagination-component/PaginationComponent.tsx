3e7930c0a77367cdb501d79871e3466b27ec32ba
Олексій2930 <korobcukdenis30@gmail.com>
2025-10-25 23:37:16 +0300

    dz4 task1

--- src/components/pagination-component/PaginationComponent.tsx
+++ src/components/pagination-component/PaginationComponent.tsx
@@ -3,7 +3,7 @@ import {useSearchParams} from "react-router-dom";
 export const PaginationComponent = () => {
 
     const [searchParams, setSearchParams] = useSearchParams({page: '1'});
-    let currentPage = Number(searchParams.get('page') || '1');
+    let currentPage = Number(searchParams.get('currentPage') || '1');
 
     return (
         <div>
