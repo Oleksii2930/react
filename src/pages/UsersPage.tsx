3426698e6ba518866ec059c6475e9fa12c34d0f5
Олексій2930 <korobcukdenis30@gmail.com>
2026-01-27 13:24:43 +0200

    dispathc

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,6 +1,5 @@
 import {useEffect} from "react";
 import {useAppSelector} from "../redux/hooks/useAppSelector.tsx";
-
 import {useAppDispatch} from "../redux/hooks/useAppDispatch.tsx";
 import {userSliceActions} from "../redux/slices/userSlice/userSlice.ts";
 
